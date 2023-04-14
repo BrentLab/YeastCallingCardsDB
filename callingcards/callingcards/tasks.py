a383600bd65ef4f8da5e0fd9c547419210db60d9
chase mateusiak <chase.mateusiak@gmail.com>
2023-04-14 18:06:49 -0500

    recofiguring how the celery tasks handle the User object when adding records

--- callingcards/callingcards/tasks.py
+++ callingcards/callingcards/tasks.py
@@ -1,6 +1,6 @@
 from django.utils.module_loading import import_string
+from django.contrib.auth import get_user_model
 from ..celery import app
-
 
 @app.task
 def process_upload(data, many_flag, kwargs):
@@ -10,24 +10,16 @@ def process_upload(data, many_flag, kwargs):
     """
     serializer_class_path = kwargs.pop("serializer_class_path")
     serializer_class = import_string(serializer_class_path)
-    
+
+    user_pk = kwargs.pop("uploader")
+    User = get_user_model()
+    user = User.objects.get(pk=user_pk)
+    kwargs["uploader"] = user
+
     if many_flag:
         serializer = serializer_class(data=data, many=True)
     else:
         serializer = serializer_class(data=data)
-    
     serializer.is_valid(raise_exception=True)
-
-    if many_flag:
-        instances = []
-        for item in serializer.validated_data:
-            # Add the 'uploader' field to each record
-            item.update(kwargs)  
-            # Manually create instances using serializer's create() method
-            instance = serializer.create(item)  
-            instances.append(instance)
-        serializer.instance = instances
-    else:
-        serializer.save(**kwargs)
-
+    serializer.save(**kwargs)
     return serializer.data
