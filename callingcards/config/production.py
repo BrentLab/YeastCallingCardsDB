b84f24ff5fd33747a59d3d7d59a30f5c01edbe62
chase mateusiak <chase.mateusiak@gmail.com>
2023-04-24 17:29:39 -0500

    updating poetry lock; dealing with deprecated static file and default file settings in conf/production

--- callingcards/config/production.py
+++ callingcards/config/production.py
@@ -14,8 +14,12 @@ class Production(Common):
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     # http://django-storages.readthedocs.org/en/latest/index.html
     INSTALLED_APPS += ('storages',)
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    STORAGES = {
+        'default': 'storages.backends.s3boto3.S3Boto3Storage',
+        'staticfiles': 'storages.backends.s3boto3.S3Boto3Storage',
+    }
+    
     AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME')
