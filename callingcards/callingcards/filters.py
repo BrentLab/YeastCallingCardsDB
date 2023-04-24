637d7088698a9bae1b4dc7aa95dc8035a50f36f6
chase mateusiak <chase.mateusiak@gmail.com>
2023-04-24 17:55:53 -0500

    fixing bug in Hops filter

--- callingcards/callingcards/filters.py
+++ callingcards/callingcards/filters.py
@@ -6,19 +6,6 @@ from .models import (Gene, McIsaacZEV, KemmerenTFKO, Background,
                      QcManualReview, QcR1ToR2Tf, QcR2ToR1Tf,
                      QcTfToTransposon)
 
-class HopsFilter(django_filters.FilterSet):
-    tf_id = django_filters.NumberFilter(field_name="experiment__tf__id")
-    tf_locus_tag = django_filters.CharFilter(field_name="experiment__tf__locus_tag")
-    tf_gene = django_filters.CharFilter(field_name="experiment__tf__gene")
-    experiment = django_filters.CharFilter(field_name="experiment__id")
-    experiment_id = django_filters.CharFilter(field_name="experiment__id")
-
-    class Meta:
-        model = Hops
-        fields = ['tf_id', 'tf_locus_tag', 'tf_gene', 
-                  'experiment', 'experiment_id']
-
-
 class BackgroundFilter(django_filters.FilterSet):
     background_source = django_filters.CharFilter(field_name="source")
 
@@ -198,6 +185,19 @@ class CCExperimentFilter(django_filters.FilterSet):
         model = CCExperiment
         fields = ['experiment', 'batch', 'batch_replicate',
                   'tf_id', 'tf_locus_tag', 'tf_gene']
+
+
+class HopsFilter(django_filters.FilterSet):
+    tf_id = django_filters.NumberFilter(field_name="experiment__tf__tf__id")
+    tf_locus_tag = django_filters.CharFilter(field_name="experiment__tf__tf__locus_tag")
+    tf_gene = django_filters.CharFilter(field_name="experiment__tf__tf__gene")
+    experiment = django_filters.CharFilter(field_name="experiment__id")
+    experiment_id = django_filters.CharFilter(field_name="experiment__id")
+
+    class Meta:
+        model = Hops
+        fields = ['tf_id', 'tf_locus_tag', 'tf_gene', 
+                  'experiment', 'experiment_id']
 
 
 class HopsReplicateSigFilter(django_filters.FilterSet):
